6f2fc1a31e32fabba55086bf89817a86a77684ea
Jonathan Darakdjian <jonydarakdjian@gmail.com>
2024-04-13 13:55:06 -0300

    Feat: Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
-import SignIn from "./pages/login/signIn";
+
+import Home from "./pages/Home";
 
 const App: React.FC = () => {
   return <>
-    <SignIn />
+    <Home />
   </>;
 };
 
